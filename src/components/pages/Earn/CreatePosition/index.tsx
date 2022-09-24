3452b368e33e75fd268151bcfb30d061aa343d3e
Robert Leifke <r.leifke@gmail.com>
2022-09-24 13:54:15 -0400

    added description

--- src/components/pages/Earn/CreatePosition/index.tsx
+++ src/components/pages/Earn/CreatePosition/index.tsx
@@ -85,7 +85,7 @@ export const CreatePosition: React.FC = () => {
               <p tw="text-xl font-semibold text-black">
                 Select tokens and amounts
               </p>
-              <p tw="text-default">Provide two-token liquidity by setting the amount and confirming the transaction</p>
+              <p tw="text-default">Provide two-token liquidity and get an LP share.</p>
             </>
           </div>
           <SelectPair />
