fa0781e1b588b662539545412dd91d151eb6fa6d
Kyle Scott <kyscott@umich.edu>
2022-12-07 14:09:10 -0500

    add banner

---
+++ src/components/pages/Trade/Notice.tsx
@@ -0,0 +1,10 @@
+export const Notice: React.FC = () => {
+  return (
+    <div tw="flex items-start gap-2 max-w-lg mb-3">
+      <div tw="flex flex-col gap-2">
+        <p tw="font-bold text-2xl">Numoen is live 🫡</p>
+        <p tw="">Go long with squared leverage and continuous liquidations</p>
+      </div>
+    </div>
+  );
+};
--- src/components/pages/Trade/index.tsx
+++ src/components/pages/Trade/index.tsx
@@ -1,10 +1,11 @@
+import { Notice } from "./Notice";
 import { Swap } from "./Swap";
 import { SwapStateProvider } from "./useSwapState";
 
 export const Trade: React.FC = () => {
   return (
     <div tw="flex flex-col gap-4">
-      {/* <Arbitrum /> */}
+      <Notice />
       <SwapStateProvider>
         <Swap />
       </SwapStateProvider>
