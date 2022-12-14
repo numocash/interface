3e34db7243a98cebd43a98761eb835c781c3f6b5
Kyle Scott <kyscott@umich.edu>
2022-12-14 11:25:44 -0500

    fix banner

--- src/components/pages/Trade/Notice.tsx
+++ src/components/pages/Trade/Notice.tsx
@@ -1,4 +1,3 @@
-import power from "../../common/images/power.png";
 import { Module } from "../../common/Module";
 
 export const Notice: React.FC = () => {
@@ -6,15 +5,13 @@ export const Notice: React.FC = () => {
     <Module tw="flex items-start gap-2 max-w-lg mb-3 border-0 bg-[#111214]">
       <div tw="flex flex-col gap-2">
         <p tw="font-bold text-2xl flex items-center gap-1 text-white">
-          Numoen has squared options{" "}
-          <span>
-            <img tw="h-[45px] w-[59px]" src={power} alt={`power`} />
-          </span>
+          Numoen has leveraged tokens!
         </p>
-        <p tw="text-white">
-          Compared to a 2x perp, you make more when prices go up and lose less
-          when prices go down and can't be liquidated
-        </p>
+        <div tw="ml-4">
+          <p tw="text-white list-item">Always 2x leverage</p>
+          <p tw="text-white list-item">No liquidations</p>
+          <p tw="text-white list-item">No oracles</p>
+        </div>
       </div>
     </Module>
   );
