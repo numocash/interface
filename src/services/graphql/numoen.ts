aff1efb86eb70d9eba2c97c6862d9e415118fb80
Kyle Scott <kyscott@umich.edu>
2023-03-17 12:43:28 -0400

    update subgraph

--- src/services/graphql/numoen.ts
+++ src/services/graphql/numoen.ts
@@ -14,10 +14,10 @@ export const parseLendgines = (
   address: Address;
 }[] => {
   return lendginesQuery.lendgines.map((l) => ({
-    token0: utils.getAddress(l.token0),
-    token1: utils.getAddress(l.token1),
-    token0Exp: l.token0Exp,
-    token1Exp: l.token1Exp,
+    token0: utils.getAddress(l.token0.id),
+    token1: utils.getAddress(l.token1.id),
+    token0Exp: +l.token0Exp,
+    token1Exp: +l.token1Exp,
     upperBound: l.upperBound,
     address: utils.getAddress(l.id),
   }));
