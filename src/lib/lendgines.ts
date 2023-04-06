53b63fe71310e17f2cfff948877d9a4a2ce53c13
Kyle Scott <kyscott@umich.edu>
2023-04-06 13:20:28 -0400

    bug fix for short and long lendgines again

--- src/lib/lendgines.ts
+++ src/lib/lendgines.ts
@@ -5,12 +5,12 @@ import type { Lendgine } from "./types/lendgine";
 export const pickLongLendgines = (
   lendgines: readonly Lendgine[],
   base: Token
-) => lendgines.filter((l) => l.token0.equals(base));
+) => lendgines.filter((l) => l.token1.equals(base));
 
 export const pickShortLendgines = (
   lendgines: readonly Lendgine[],
   base: Token
-) => lendgines.filter((l) => l.token1.equals(base));
+) => lendgines.filter((l) => l.token0.equals(base));
 
 export const isLongLendgine = (lendgine: Lendgine, base: Token) =>
   lendgine.token1.equals(base);
